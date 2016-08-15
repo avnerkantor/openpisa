95d87a982edd4aae866f91b378d6f75c93c102e4
avnerkantor <avnerkantor@gmail.com>
2016-08-15 16:30:56 +0300

    -

--- pisa.scores.R
+++ pisa.scores.R
@@ -1,6 +1,6 @@
 #choices = list("combined" = "combined", "male only" = "male", "female only" = "female")
 
-load("data/pisaData1.rda")
+load("data/pisaData2.rda")
 
 #observe({
 #  if(input$calibrationCheck){
@@ -25,18 +25,18 @@ load("data/pisaData1.rda")
 #todo לעשות הודעת שגיאה של לא נבחנו במקצוע. אם אין מידע
 observe({
   if (input$worldOrIsrael=="World") 
-      {
-            updateSelectInput(session, "Country1", choices = names(oecdList), selected = "ישראל")
-            updateSelectInput(session, "Country2", choices = names(oecdList), selected = "בריטניה")
-            updateSelectInput(session, "Country3", choices = names(oecdList), selected = "פינלנד")
-            updateSelectInput(session, "Country4", choices = names(oecdList), selected = "דרום-קוריאה")
-          } else {
-            updateSelectInput(session, "Country1", choices = names(israelList), selected = "חינוך-ממלכתי")
-            updateSelectInput(session, "Country2", choices = names(israelList), selected = "ממלכתי-דתי")
-            updateSelectInput(session, "Country3", choices = names(israelList), selected = "חרדים-בנות")
-            updateSelectInput(session, "Country4", choices = names(israelList), selected = "דוברי ערבית")
-          }
-  })
+  {
+    updateSelectInput(session, "Country1", choices = names(oecdList), selected = "ישראל")
+    updateSelectInput(session, "Country2", choices = names(oecdList), selected = "בריטניה")
+    updateSelectInput(session, "Country3", choices = names(oecdList), selected = "פינלנד")
+    updateSelectInput(session, "Country4", choices = names(oecdList), selected = "דרום-קוריאה")
+  } else {
+    updateSelectInput(session, "Country1", choices = names(israelList), selected = "חינוך-ממלכתי")
+    updateSelectInput(session, "Country2", choices = names(israelList), selected = "ממלכתי-דתי")
+    updateSelectInput(session, "Country3", choices = names(israelList), selected = "חרדים-בנות")
+    updateSelectInput(session, "Country4", choices = names(israelList), selected = "דוברי ערבית")
+  }
+})
 
 observe({
   
@@ -44,8 +44,8 @@ observe({
     select(Level, contains(input$Subject))%>%
     filter(!is.na(input$Subject))
   
-  plotData1<-pisaData1%>%filter(Subject==input$Subject, Performers==0)%>%select(-Subject, -Performers)
-
+  plotData1<-pisaData2%>%filter(Subject==input$Subject, Performers==0)%>%select(-Subject, -Performers)
+  
   if (!is.null(input$Gender)){
     if(!is.null(input$Escs)){
       plotData2<- plotData1 %>%
@@ -62,56 +62,48 @@ observe({
         filter(Gender == 0)%>%
         filter(ESCS %in% c(input$Escs))
     } else {
-    plotData2<-plotData1 %>%
-      filter(Gender==0, ESCS==0)
-   }
+      plotData2<-plotData1 %>%
+        filter(Gender==0, ESCS==0)
+    }
   }
-
+  
   #output$table1 <- renderTable({
-    #SubjectExpertiseLevelsBreaks
-   # plotData
+  #SubjectExpertiseLevelsBreaks
+  # plotData
   #})
   
-  #color Palettes
-  # generalPalette <- c("#7b3a96", "#bc99c7", "#b276b2")
-  # girlsPalette<-c("#e5126f", "#f17cb0", "#f6aac9")
-  # boysPalette<-c("#265dab", "#5da5da", "#88bde6")
- 
   scoresPlotFunction<-function(country){
     plotData3 <- plotData2%>%
       filter(Country==
-        as.vector(unlist(Countries%>%filter(Hebrew==country)%>%select(CNT))))
-    #%>%select(-Country)
+               as.vector(unlist(Countries%>%filter(Hebrew==country)%>%select(CNT))))%>%select(-Country)
+
     #ggplot(plotData3, aes(x=Year, y=Average, colour=ESCS, group=interaction(ESCS, Gender))) +
-    ggplot(plotData3, aes(x=Year, y=Average, colour=ESCS, fill=Gender)) +
+    #ggplot(plotData3, aes(x=Year, y=Average, colour=ESCS, fill=Gender)) +    
+      ggplot(plotData3, aes(x=Year, y=Average, colour=GenderESCS)) +
       geom_line() +
-      scale_colour_manual("Male", values = c("#265dab", "#5da5da", "#88bde6")) +
-      scale_fill_manual("Female", values = c("#7b3a96", "#bc99c7", "#b276b2")) +
-      #      scale_fill_manual(values=c("Male" = boysPalette, "Female"=girlsPalette)) +
+      scale_colour_manual(values = c("General"="#b276b2", "Male"="#5da5da", "Female"="#f17cb0", "GeneralLow"="#bc99c7", "GeneralMedium"="#b276b2", "GeneralHigh"="#7b3a96", "MaleHigh"="#265dab", "MaleLow"="#88bde6", "MaleMedium"="#5da5da", "FemaleHigh"="#e5126f", "FemaleLow"="#f6aac9", "FemaleMedium"="#f17cb0")) +
       theme_bw()+
       guides(colour=FALSE) +
       scale_x_discrete(breaks=c(2006,2009,2012), limits=c(2006,2009, 2012)) +
       scale_y_continuous(breaks=SubjectExpertiseLevels[2], labels=SubjectExpertiseLevels[1], 
-          limits = c(min(SubjectExpertiseLevels[2], na.rm = TRUE), 
-                     max(SubjectExpertiseLevels[2], na.rm = TRUE))) +
-      labs(title=" ", y="רמה" ,x= " ") 
-    
-    #print(min(SubjectExpertiseLevels[2], na.rm = TRUE))
+                         limits = c(min(SubjectExpertiseLevels[2], na.rm = TRUE), 
+                                    max(SubjectExpertiseLevels[2], na.rm = TRUE))) +
+      labs(title=" ", y="" ,x= " ") 
   }
   
-  output$pisaScoresTable <- DT::renderDataTable(options=list(
-    pageLength = 5,
-    searching=FALSE,
-    autoWidth = TRUE
-  ), rownames= FALSE,
-  {
-    plotData3<-plotData1%>%
-      filter(Country=="ISR")%>%filter(Gender %in% c(input$Female, input$Male))
-
-  })
-  output$text1 <- renderText(
-    paste(c(input$Female))
-  )
+  # output$pisaScoresTable <- DT::renderDataTable(options=list(
+  #   pageLength = 5,
+  #   searching=FALSE,
+  #   autoWidth = TRUE
+  # ), rownames= FALSE,
+  # {
+  #   plotData3<-plotData1%>%
+  #     filter(Country=="ISR")%>%filter(Gender %in% c(input$Female, input$Male))
+  #   
+  # })
+  # output$text1 <- renderText(
+  #   paste(c(input$Female))
+  # )
   output$Country1Plot<-renderPlot({
     scoresPlotFunction(input$Country1)
   })
@@ -136,9 +128,7 @@ observeEvent(input$Subject,{
   
   updateNumericInput(session, "levelNumber", "", min=minLevel, max=maxLevel, value=3, step=1)
   
-  
-  
   output$ExplenationTable <- renderTable({
-  LevelExplenation%>%filter(Subject==input$Subject, Level==input$levelNumber)%>%select(Explenation)
+    LevelExplenation%>%filter(Subject==input$Subject, Level==input$levelNumber)%>%select(Explenation)
   }, include.rownames=FALSE, include.colnames=FALSE)
 })
